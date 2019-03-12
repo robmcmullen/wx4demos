033ac62e595a7b0c78f0410fd7033506b5d92277
Rob McMullen <feedback@playermissile.com>
2019-03-12 04:42:02 -0700

    Updated scrolled-wrap-sizer test to use size reporter control

--- scrolled-wrap-sizer.py
+++ scrolled-wrap-sizer.py
@@ -2,15 +2,52 @@ import wx
 import wx.lib.scrolledpanel as scrolled
 
 
-text = "one two buckle my shoe three four shut the door five six pick up sticks seven eight lay them straight nine ten big fat hen"
+class SizeReportCtrl(wx.Control):
+    def __init__(self, parent, id=wx.ID_ANY, pos=wx.DefaultPosition,
+                size=wx.DefaultSize):
+        wx.Control.__init__(self, parent, id, pos, size, style=wx.NO_BORDER)
+
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Bind(wx.EVT_ERASE_BACKGROUND, self.on_erase_background)
+        self.Bind(wx.EVT_SIZE, self.on_size)
+
+    def on_paint(self, event):
+        dc = wx.PaintDC(self)
+        size = self.GetClientSize()
+        dc.SetFont(wx.NORMAL_FONT)
+        dc.SetBrush(wx.WHITE_BRUSH)
+        dc.SetPen(wx.WHITE_PEN)
+        dc.DrawRectangle(0, 0, size.x, size.y)
+        dc.SetPen(wx.LIGHT_GREY_PEN)
+        dc.DrawLine(0, 0, size.x, size.y)
+        dc.DrawLine(0, size.y, size.x, 0)
+
+        print(f"on_paint: id={self.GetId()}")
+
+        s = f"Id: {self.GetId()}"
+        w, height = dc.GetTextExtent(s)
+        height += 3
+        dc.DrawText(s, (size.x-w)/2, (size.y//2 - height))
+
+        s = f"Size: {size.x} x {size.y}"
+        w, height = dc.GetTextExtent(s)
+        height += 3
+        dc.DrawText(s, (size.x-w)/2, (size.y//2 + height))
+
+    def on_erase_background(self, event):
+        pass
+
+    def on_size(self, event):
+        self.Refresh()
+
 
 class ScrolledWrapPanel(scrolled.ScrolledPanel):
     def __init__(self, parent):
         scrolled.ScrolledPanel.__init__(self, parent, -1)
 
         wsizer = wx.WrapSizer(orient=wx.HORIZONTAL)
-        for word in text.split():
-            btn = wx.Button(self, -1, word, size=(200,100))
+        for i in range(1000):
+            btn = SizeReportCtrl(self, -1, size=(200,100))
             wsizer.Add(btn, 0, wx.ALL, 4)
 
         self.SetSizer(wsizer)
